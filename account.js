9323d9e95409e68e8b8f8849631616ed04f148f3
aleksanderbaishev <160151645+aleksanderbaishev@users.noreply.github.com>
2025-03-23 20:30:47 +0300

    Update account.js

--- account.js
+++ account.js
@@ -1,5 +1,5 @@
-const API_KEY = '376d83d3-a102-4e83-9973-0bbfa8230233';
-const BASE_URL = 'http://cat-facts-api.std-900.ist.mospolytech.ru/api';
+const API_KEY = '737a2d8a-5732-4983-a109-60dfb5a6fde4';
+const BASE_URL = 'http://exam-api-courses.std-900.ist.mospolytech.ru/api';
 
 // Функция для отображения уведомлений
 function showNotification(message, type = 'success') {
